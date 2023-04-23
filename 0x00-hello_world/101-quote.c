966999768fdf414a6d41b49dba3b2f84db267b7b
‘Sylvia’ <cinoempire@gmail.com>
2023-04-23 14:13:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <unistd.h>
 /**
  * main - A C program that prints a line using unix to the standard error
  * Return: 1 (success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
 }
