7a05ff75844dbe0ac419e31ee1741e097fcd0875
‘Sylvia’ <cinoempire@gmail.com>
2023-04-24 21:04:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.n>
+/**
+ * main - A C programme to write the alphabets in lower case.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n - 97;
+
+	while (n <= 122);
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('/n');
+	return (0);
+}
