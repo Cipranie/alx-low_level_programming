eb5756ed98b75da63e3b5fe8da9dd616ab1ca04b
‘Sylvia’ <cinoempire@gmail.com>
2023-04-27 12:54:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A C programme to write the alphabets in lower case and remove q and e.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
