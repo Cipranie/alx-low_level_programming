d6fa99ec095a3c4cf56ad2c03d594fe748766b78
‘Sylvia’ <cinoempire@gmail.com>
2023-04-27 13:10:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A C programme that prints all single
+ * digit on base 10 starting from 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
