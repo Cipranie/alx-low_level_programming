ad079817adcd4442cfa544779e9050f5fa6b1d01
‘Sylvia’ <cinoempire@gmail.com>
2023-04-28 18:50:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *print_numbers - A program that prints numbers 0 - 9
+  *Return: void
+  */
+
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 48; d <= 57; d++)
+	{
+	putchar(d);
+	}
+	putchar('\n');
+
+}
