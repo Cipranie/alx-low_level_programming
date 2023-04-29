78cc5a5be1ec5a8298f1e8f6a63e80240d3a9f4c
‘Sylvia’ <cinoempire@gmail.com>
2023-04-28 17:46:47 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,30 +3,20 @@
 
 /**
   * _isupper - checks for uppercase character
-  * @c - character to check
+  * @c: - character to check
   * Return: 1 (success)
   */
- 
+
 int _isupper(int c)
 {
-	/*
-	int s;
-
-	for (s=65; s<=90; s++)
-	{
-		if (c==s)
-			return 1;
-	}
-	return 0;
-	*/
 
 	char ch;
 
-	for (ch='A'; ch<='Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
-		if (c==ch)
-			return 1;
+		if (c == ch)
+			return (1);
 	}
-	return 0;
-	
+	return (0);
+
 }
