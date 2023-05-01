026546cf39688ad33245c2673a75a42a8995d693
‘Sylvia’ <cinoempire@gmail.com>
2023-05-01 03:33:06 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
