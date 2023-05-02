d79f64c0c32afa9c507b1e88cf50bcf6e57079a8
‘Sylvia’ <cinoempire@gmail.com>
2023-05-02 16:27:36 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - A program that reassign an int value
+  * to 98 using pointers
+  * @n: variable name
+  */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
